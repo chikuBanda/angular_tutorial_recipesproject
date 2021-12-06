1ba5e02dc6fca46f6b7df36f9952818326c1fb93
chiku banda <chikubanda@pop-os.localdomain>
2021-12-06 19:52:13 +0100

    implemented instructors approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
-import { RecipeDetailComponent } from './content/recipe-list/recipe-item/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './content/recipe-list/recipe-item/recipe-item.component';
-import { RecipeComponentList } from './content/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './content/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './content/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeComponentList } from './content/recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './content/recipes/recipes.component';
 import { ShoppingListEditComponent } from './content/shopping/shopping-list-edit/shopping-list-edit.component';
 import { ShoppingListComponent } from './content/shopping/shopping-list/shopping-list.component';
 import { ShoppingComponent } from './content/shopping/shopping.component';
@@ -14,14 +15,15 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ContentComponent,
-    RecipeComponentList,
-    RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingComponent,
+    RecipeItemComponent,
+    RecipeComponentList,
+    ShoppingListEditComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent
+    ShoppingComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
